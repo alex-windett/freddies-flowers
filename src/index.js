c12014068ccd2cca89891db813d7511d446ae9b3
Alex Windett <windettalex@gmail.com>
2016-04-06 19:23:23 +0100

    basic setup for react app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './scss/app'
+
+import React        from 'react';
+import ReactDOM     from 'react-dom'
+
+import App          from 'js/app'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
