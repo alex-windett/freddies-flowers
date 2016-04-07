4fa3ffa08f956db985b69c9a1ef8ee7298dda274
Alex Windett <alex@bigfish.co.uk>
2016-04-07 16:22:37 +0100

    adding in new adress field

--- src/js/components/deliveryAddress/DeliveryAddress.js
+++ src/js/components/deliveryAddress/DeliveryAddress.js
@@ -4,6 +4,13 @@ import Actions from '../../actions/deliveryAddressActions'
 
 const AddressItem = React.createClass({
 
+    getInitialState() {
+
+        return {
+            formVisibility: 'hide'
+        }
+    },
+
     editAddress() {
         Actions.editAddress()
     },
@@ -12,29 +19,65 @@ const AddressItem = React.createClass({
         Actions.cancelAddress()
     },
 
+    addAddress() {
+
+    },
+
+    toggleFormVisibility(e = '') {
+        if ( e != '') {
+            e.preventDefault()
+        }
+        
+        if ( this.state.formVisibility) {
+            this.setState({ formVisibility: '' })
+        } else {
+            this.setState({ formVisibility: 'hide' })
+        }
+    },
+
     render() {
 
         return (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>First Line of address</td>
-                        <td>Quantity</td>
-                        <td>Total cost</td>
-                        <td>Manage</td>
-                    </tr>
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>First Line of address</td>
+                            <td>Quantity</td>
+                            <td>Total cost</td>
+                            <td>Manage</td>
+                        </tr>
 
-                    <tr>
-                        <td>9 Takhar Mews</td>
-                        <td>1 box</td>
-                        <td>20</td>
-                        <td>
-                            <button className="button" onClick={this.editAddress}>Edit</button>
-                            <button className="button alert" onClick={this.cancelAddress}>Cancel</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                        <tr>
+                            <td>9 Takhar Mews</td>
+                            <td>1 box</td>
+                            <td>20</td>
+                            <td>
+                                <button className="button" onClick={this.editAddress}>Edit</button>
+                                <button className="button alert" onClick={this.cancelAddress}>Cancel</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <button className="button success" onClick={this.toggleFormVisibility}>Add an new delivery address</button>
+
+                <form className={this.state.formVisibility + " form form__addAdress"} method="post">
+                    <label>Postcode:</label>
+                    <input type="text"/>
+                    <label>House name or number</label>
+                    <input type="text"/>
+                    <label>Street Name</label>
+                    <input type="text"/>
+                    <label>Town or City</label>
+                    <input type="text"/>
+
+                    <button className="button success" onClick={this.addAddress}>Sumbit</button>
+                    <button className="button alert" onClick={this.toggleFormVisibility}>Cancel</button>
+
+                    <p className="text-center">Enter your postcode to calculate delivery charge</p>
+                </form>
+            </div>
         )
     }
 })
