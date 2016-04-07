4e899df8a96e9d357642642efac07a89cb0de609
Alex Windett <alex@bigfish.co.uk>
2016-04-07 09:54:15 +0100

    toggling states for upcoming deliveries

--- src/js/components/delivery/Delivery.js
+++ src/js/components/delivery/Delivery.js
@@ -2,11 +2,41 @@ import React from 'react'
 
 const DeliveryItem = React.createClass({
 
+    getInitialState() {
+
+        return {
+            active: true
+        }
+    },
+
+    toggleState() {
+        if ( this.state.active ) {
+            this.setState({ active: false })
+        } else {
+            this.setState({ active: true })
+        }
+    },
+
     render() {
+        if ( this.state.active ) {
+            var activeClass = 'secondary'
+            var activeText  = 'SKIP'
+
+            var textStyles = {
+                textDecoration: 'none'
+            }
+        } else {
+            var activeClass = 'success'
+            var activeText  = 'ACTIVATE'
+
+            var textStyles = {
+                textDecoration: 'line-through'
+            }
+        }
 
         return (
             <div>
-                <h4>Friday 4 September</h4> <button className="button">SKIP</button>
+                <h4 style={textStyles}>Friday 4 September</h4> <button className={"button " + activeClass} onClick={this.toggleState}>{activeText}</button>
 
                 <hr />
             </div>
