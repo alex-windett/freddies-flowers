d4543cce9c8c8a5ccf7ccb395b98122c1b206a77
Alex Windett <alex@bigfish.co.uk>
2016-04-11 15:42:11 +0100

    finishing initial style guide

--- webpack.config.js
+++ webpack.config.js
@@ -74,104 +74,12 @@ const config = {
     ],
     postcss: [
         autoprefixer({
-            browsers: ['last 2 versions']
+            browsers: [
+                'last 2 versions',
+                'ie >= 9'
+            ]
         })
     ]
 }
 
 module.exports = config
-
-
-// const ExtractTextPlugin = require('extract-text-webpack-plugin')
-// const path              = require('path')
-// const LiveReloadPlugin  = require('webpack-livereload-plugin')
-// const NpmInstallPlugin = require('npm-install-webpack-plugin')
-//
-// // const autoprefixer      = require('autoprefixer')
-// const buildPath         = './build'
-// const TARGET            = process.env.npm_lifecycle_event;
-//
-// const PATHS = {
-//   src: path.join(__dirname, './src'),
-//   build: path.join(__dirname, './build'),
-//   style: path.join(__dirname, '.src/scss')
-// };
-//
-// const sassLoaders = [
-//     'style-loader',
-//     'css-loader',
-//     'style!css!sass?includePaths[]=' +
-//         path.resolve(__dirname,
-//             './bower_components/foundation/scss'
-//         )
-// ]
-//
-// const config = {
-//     entry: {
-//         app: './src/index.js'
-//     },
-//     output: {
-//         filename: '[name].js',
-//         path: path.join(__dirname, buildPath),
-//         publicPath: buildPath
-//     },
-//     resolve: {
-//         root: path.resolve(__dirname),
-//         modulesDirectories: [
-//             './src',
-//             './node_modules',
-//             './bower_components'
-//             // './bower_components/foundation-sites/scsss'
-//           ],
-//         extensions: ['', '.js', '.scss', '.sass']
-//     },
-//     module: {
-//         loaders: [
-//             {
-//                 test: [/\.js$/, /\.es6$/],
-//                 exclude: /node_modules|bower_components/,
-//                 loader: "babel-loader",
-//                 query: {
-//                     presets: ['es2015', 'react']
-//                 }
-//             },
-//                 test: /\.scss$/,
-//                 loader: ExtractTextPlugin.extract("style-loader", "css-loader!sass-loader")
-//             {
-//                 test: /\.(eot|svg|ttf|woff|woff2)$/,
-//                 // Add the fonts to a public folder in build
-//                 loader: 'file?name=/fonts/[name].[ext]'
-//             },
-//             {
-//                 // Images are inline within the JS
-//                 test: /.*\.(gif|png|jpe?g|svg)$/i,
-//                 loaders: [
-//                     'file?hash=sha512&digest=hex&name=[hash].[ext]',
-//                     'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
-//                 ]
-//             }
-//         ]
-//     },
-//     // sassLoader: {
-//     //     includePaths: [
-//     //         path.resolve(__dirname),
-//     //         path.resolve(__dirname, './src'),
-//     //         path.join(__dirname, 'node_modules'),
-//     //         path.join(__dirname, './bower_components'),
-//     //         path.join(__dirname, './bower_components/foundation-sites/scss')
-//     //
-//     //     ]
-//     // },
-//     devtool: "inline-source-map", // or "inline-source-map"
-//     watch: true
-//     // plugins: [
-//     //     new NpmInstallPlugin({
-//     //         save: true // --save
-//     //     }),
-//     //     new ExtractTextPlugin('[name].css'),
-//     //     // new ExtractTextPlugin('[name].css', { allChunks: false }),
-//     //     new LiveReloadPlugin()
-//     // ],
-// }
-//
-// module.exports = config
