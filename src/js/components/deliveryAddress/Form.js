6a4223b24b11f56a21bd3b45b5ecdd66e6f04e59
Alex Windett <alex@bigfish.co.uk>
2016-04-13 14:59:16 +0100

    validation on form and some styling

--- src/js/components/deliveryAddress/Form.js
+++ src/js/components/deliveryAddress/Form.js
@@ -17,13 +17,14 @@ var Input = React.createClass({
         this.setValue(event.currentTarget.value);
     },
     render: function () {
-        var className = this.showRequired() ? 'input__required' : this.showError() ? 'input__error' : null;
+        const additionalClasses = this.props.className ? ' ' + this.props.className : ''
+        const className         = this.showRequired() ? 'input__required' : this.showError() ? 'input__error' : null
 
         return (
-            <div className="input">
+            <div className={'input' + additionalClasses}>
                 <input className={className} type="text" placeholder={this.props.placeholder} onChange={this.changeValue} value={this.getValue()}/>
 
-                <span className="input__error--description">{this.getErrorMessage()}</span>
+                {/*<span className="input__error--description">{this.getErrorMessage()}</span>*/}
             </div>
         );
     }
@@ -38,32 +39,69 @@ const MyAppForm = React.createClass({
             canSubmit: false
         }
     },
+
     enableButton () {
         console.log('able to submit')
         this.setState({
             canSubmit: true
         });
     },
+
     disableButton () {
         console.log('unable to submit')
         this.setState({
             canSubmit: false
         });
     },
-    submit (model){
 
+    submit(model){
+        /**
+        * @param {model} contains the fields and their value of submitted form
+        */
+        debugger
 
     },
+
     render () {
+        const regex = /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/
+
         return (
-            <Formsy.Form onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
-                <Input placeholder="Email Address"
-                    name="email"
+            <Formsy.Form className="clearfix" onSubmit={this.submit} onValid={this.enableButton} onInvalid={this.disableButton}>
+                <Input className="input__left"
+                    placeholder="Postcode"
+                    name="postcode"
                     validations={{
-                        isEmail: true,
+                        minLength: 4,
+                        maxLength: 6,
                         isExisty: true
                     }}
-                    validationError="This is not a valid email"
+                    validationError="This is not a valid postcode"
+                    required/>
+                <button className="button button__secondary float-left">Find Address</button>
+
+                <Input className="input__left"
+                    placeholder="House Number or Name"
+                    name="house"
+                    validations={{
+                        isAlphanumeric: true,
+                        isExisty: true
+                    }}
+                    required/>
+                <Input className="input__left"
+                    placeholder="Street Name"
+                    name="street"
+                    validations={{
+                        isAlpha: true,
+                        isExisty: true
+                    }}
+                    required/>
+                <Input className="input__right"
+                    placeholder="Town or City"
+                    name="city"
+                    validations={{
+                        isAlpha: true,
+                        isExisty: true
+                    }}
                     required/>
                 <button type="submit"  disabled={!this.state.canSubmit} className="button button__primary" >Submit</button>
             </Formsy.Form>
