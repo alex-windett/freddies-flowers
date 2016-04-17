1e26dd63b200771f208e5b6f825a895adc7218c5
Alex Windett <windettalex@gmail.com>
2016-04-17 12:17:28 +0100

    fixing imagmine webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -59,8 +59,8 @@ if ( TARGET === 'start' || TARGET === 'watch' ) {
                     // Images are inline within the JS
                     test: /.*\.(gif|png|jpe?g|svg)$/i,
                     loaders: [
-                        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                        'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+                        'file?hash=sha512&digest=hex&name=images/[name].[ext]',
+                        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
                     ]
                 }
             ]
