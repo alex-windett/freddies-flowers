60a019e338412a5eedf116d33e555cf6848fdf7c
Alex Windett <alex@bigfish.co.uk>
2016-05-05 11:40:00 +0100

    adding in hiding of card number

--- src/js/components/bankDetails/BankDetails.js
+++ src/js/components/bankDetails/BankDetails.js
@@ -30,7 +30,6 @@ class BankDetails extends React.Component {
         })
     }
 
-
     componentDidMount() {
         Store.on(GlobalConstant.CHANGE_EVENT, _ => {
             this.setState({
@@ -42,29 +41,39 @@ class BankDetails extends React.Component {
     render() {
         const addresses = this.state.bankDetails.addresses.map( a => {
 
-            return (
-                <option key={a.id}>{a.address}</option>
-            )
+            return <option key={a.id}>{a.address}</option>
         })
-
 
         const activeCard = this.state.bankDetails.cards.filter( c => {
             if ( c.active ) return c
         })
+        
+        var str = []
+        //Create the "*" for exactly 4 digits short
+        for( var i = 1; i <= activeCard[0].cardNumber.length - 4; i++) {
+            str += "*";
+        }
+        //Join the asterisk and last 4 characters
+        var ecard = str + activeCard[0].cardNumber.substr(activeCard[0].cardNumber.length-4);
 
         return (
             <div className="decoration decoration__paper decoration__tape">
                 <h2 className="text-center">Manage your payment details</h2>
 
                 <Formsy.Form onValidSubmit={this.submit} onValid={this.enableButton.bind(this)} onInvalid={this.disableButton.bind(this)}>
+                    <label>Card Number</label>
+
+                    <input type="text" pattern="[0-9]{13,16}" title="A credit card number" placeholder={ecard}/>
+
+                    <button className="button button__secondary">Edit</button>
+                </Formsy.Form>
+
+                <Formsy.Form onValidSubmit={this.submit} onChange={this.enableButton.bind(this)} >
                     <DropdownSelect name="address">
                         {addresses}
                     </DropdownSelect>
 
-                    <label>Card Number</label>
-                    <input type="text" pattern="[0-9]{13,16}" title="A credit card number" placeholder={activeCard[0].cardNumber}/>
-                    
-                    <button className="button button__primary">Edit</button>
+                    <button className="button button__secondary" disabled={!this.state.canSubmit}>Edit</button>
                 </Formsy.Form>
             </div>
         )
