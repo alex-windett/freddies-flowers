3022ce8c5a0bd820fe9f38cee362e7268723e085
Alex Windett <alex@bigfish.co.uk>
2016-05-06 14:28:51 +0100

    adding in sticky nav

--- src/js/custom/functions/header.js
+++ src/js/custom/functions/header.js
@@ -1,15 +1,34 @@
 const header = {
 
     init() {
-        this.burgerMenu();
-        this.cloneUseraccess();
+        this.burgerMenu()
+        this.stickyNav()
     },
 
     burgerMenu() {
+        const header            = $('.header__main')
+        const stickyClass       = 'header__main--sticky'
+        const homeLogo          = $('.homelogo')
+        const stickyHomeLogo    = 'homelogo--sticky'
+
+        $(window).scroll(function() {
+            if( $(this).scrollTop() > 0 ) {
+                header.addClass(stickyClass)
+                homeLogo.addClass(stickyHomeLogo)
+            } else {
+                header.removeClass(stickyClass)
+                homeLogo.removeClass(stickyHomeLogo)
+            }
+        })
+
         $('#burgerMenu').on('click', e => {
             e.preventDefault
             $('#burgerMenuItems').slideToggle()
         })
+    },
+
+    stickyNav() {
+
     },
 }
 
