bf8fd37e1f8aa52989b6a423241332a2459e85f7
Alex Windett <alex@bigfish.co.uk>
2016-05-10 15:38:40 +0100

    adding Formsy to account details form

--- src/js/components/accountDetails/AccountDetails.jsx
+++ src/js/components/accountDetails/AccountDetails.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 
+import Formsy from 'formsy-react'
+
 import Store from '../../stores/accountDetailsStore'
 import Actions from '../../actions/accountDetailsActions'
+
+var Input = React.createClass({
+
+    // Add the Formsy Mixin
+    mixins: [Formsy.Mixin],
+
+    // setValue() will set the value of the component, which in
+    // turn will validate it and the rest of the form
+    changeValue: function (event) {
+        this.setValue(event.currentTarget.value);
+    },
+    render: function () {
+        const additionalClasses = this.props.className ? ' ' + this.props.className : ''
+        const className         = this.showRequired() ? 'input__required' : this.showError() ? 'input__error' : null
+
+        return (
+            <div className={'input' + additionalClasses}>
+                <input className={className} type="text" placeholder={this.props.placeholder} onChange={this.changeValue} value={this.getValue()}/>
+
+                {/*<span className="input__error--description">{this.getErrorMessage()}</span>*/}
+            </div>
+        );
+    }
+})
 
 class AccountDetails extends React.Component {
 
@@ -9,7 +35,9 @@ class AccountDetails extends React.Component {
         super()
 
         this.state = {
-            accountDetails: Store.getAccount()
+            accountDetails: Store.getAccount(),
+            disabled: '',
+            canSubmit: '',
         }
     }
 
@@ -21,6 +49,26 @@ class AccountDetails extends React.Component {
         })
     }
 
+    enableButton () {
+        this.setState({
+            canSubmit: true
+        });
+    }
+
+    disableButton () {
+        this.setState({
+            canSubmit: false
+        });
+    }
+
+    submit(model){
+        /**
+        * @param {model} contains the fields and their value of submitted form
+        */
+        Actions.addAddress(model)
+
+    }
+
     render() {
         const account = this.state.accountDetails
 
@@ -28,24 +76,55 @@ class AccountDetails extends React.Component {
             <div className="decoration decoration__paper decoration__tape decoration__tape--left">
                 <h2 className="text-center">Change your account details</h2>
 
-                <form method="post">
-                    <label>First name</label>
-                    <input type="text" defaultValue={account.fname}/>
+                    <Formsy.Form
+                        className="clearfix"
+                        onSubmit={this.submit}
+                        onValid={this.enableButton.bind(this)}
+                        onInvalid={this.disableButton.bind(this)} >
 
-                    <label>Last name</label>
-                    <input type="text" defaultValue={account.lname}/>
+                        <Input
+                            value={account.fname}
+                            name="name"
+                            required
+                            validations={{
+                                isExisty: true
+                            }}
+                            />
 
-                    <label>Email</label>
-                    <input type="email" defaultValue={account.email}/>
-
-                    <label>Password</label>
-                    <input type="password" defaultValue={account.password}/>
-
-                    <label>Phone Number</label>
-                    <input type="number" defaultValue={account.phone}/>
-
-                    <button className="button button__primary">Update</button>
-                </form>
+                        <Input
+                            value={account.lname}
+                            name="house"
+                            required
+                            validations={{
+                                isExisty: true
+                            }}
+                            />
+                        <Input
+                            value={account.email}
+                            name="street"
+                            required
+                            validations={{
+                                isExisty: true
+                            }}
+                            />
+                        <Input
+                            value={account.password}
+                            name="city"
+                            required
+                            validations={{
+                                isExisty: true
+                            }}
+                            />
+                        <Input
+                            value={account.phone}
+                            name="city"
+                            required
+                            validations={{
+                                isExisty: true
+                            }}
+                            />
+                        <button type="submit" disabled={!this.state.canSubmit} className="button button__primary" >Update</button>
+                    </Formsy.Form>
             </div>
         )
     }
