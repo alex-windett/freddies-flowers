766723ecbd54e967e9e3e903c2ff1180881f0c2f
Alex Windett <alex@bigfish.co.uk>
2016-05-10 11:18:19 +0100

    adding description for helper function

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,12 +1,18 @@
 const helpers = {
 
+    /**
+    * @param source - array of bojects to filter through
+    * @param id     - the ID of the item to be returned
+    *
+    * Returns object in array where ID's match
+    */
     findById(source, id) {
         for (var i = 0; i < source.length; i++) {
             if (source[i].id === id) {
                 return source[i]
             }
         }
-        throw "Couldn't find object with id: " + id
+        throw `Couldn't find object with id: ${id}`
     }
 }
 
