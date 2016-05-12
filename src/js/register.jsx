86b77691381ddfca5499f8332fca4a627f6640f0
Alex Windett <alex@bigfish.co.uk>
2016-05-12 15:58:15 +0100

    spliting up react components so they are render on different pages

---
+++ src/js/register.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Register extends React.Component {
+
+    constructor() {
+        super()
+    }
+
+    render() {
+
+        return (
+            <h1>This is the sign up form</h1>
+        )
+    }
+}
+
+export default Register
