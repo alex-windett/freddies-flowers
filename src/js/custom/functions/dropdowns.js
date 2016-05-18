90560ada95eaf319479b49ebdb41ebc7f315545e
Alex Windett <alex@bigfish.co.uk>
2016-05-18 11:53:14 +0100

    creating custom dropdown select

--- src/js/custom/functions/dropdowns.js
+++ src/js/custom/functions/dropdowns.js
@@ -2,6 +2,29 @@ const dropkick = {
 
     init() {
         $('.custom-dropdown').dropkick()
+
+        //TOGGLING NESTED ul
+        $(".dropdown__link--selected").click(function(e) {
+            e.preventDefault()
+            $(".dropdown__options--list").toggle();
+        });
+
+        //SELECT OPTIONS AND HIDE OPTION AFTER SELECTION
+        $(".dropdown__item--link").click(function() {
+            var text = $(this).html();
+
+            $(".dropdown__selected--text").html(text);
+            $(".dropdown__options--list").hide();
+        });
+
+
+        //HIDE OPTIONS IF CLICKED ANYWHERE ELSE ON PAGE
+        $(document).bind('click', function(e) {
+            e.preventDefault()
+            var $clicked = $(e.target);
+            if (! $clicked.parents().hasClass("dropdown"))
+                $(".dropdown__options--list").hide();
+        });
     }
 }
 
