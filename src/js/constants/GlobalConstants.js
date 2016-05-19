cee89087ba8a1ff107e7a544ec761ab7d92e5b8c
Alex Windett <alex@bigfish.co.uk>
2016-05-19 14:47:37 +0100

    aligning indentation

--- src/js/constants/GlobalConstants.js
+++ src/js/constants/GlobalConstants.js
@@ -1,10 +1,10 @@
 const GlobalConstants = {
 
-    CHANGE_EVENT: 'change',
-    MAX_ORDERS: 4,
-    MIN_ORDERS: 1,
-    MIN_ADDRESSES: 1,
-    MAX_DELIVERIES_PER_PAGE: 5,
+    CHANGE_EVENT            : 'change',
+    MAX_ORDERS              : 4,
+    MIN_ORDERS              : 1,
+    MIN_ADDRESSES           : 1,
+    MAX_DELIVERIES_PER_PAGE : 5,
 }
 
 export default GlobalConstants
