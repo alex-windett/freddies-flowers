e41bb4ea18814fbf1c851cb3daaac12a635c66fe
Alex Windett <alex@bigfish.co.uk>
2016-05-20 10:48:49 +0100

    refactoring stateless components by making them pure functions

--- src/js/components/formElements/BillingAddressInputs.jsx
+++ src/js/components/formElements/BillingAddressInputs.jsx
@@ -3,56 +3,49 @@ import React        from 'react'
 import Formsy       from 'formsy-react'
 import Input        from './Input'
 
-class BillingAddressInputs extends React.Component {
+const BillingAddressInputs = _ => {
 
-    constructor() {
-        super()
+    const buttonAdditionalStyle = {
+        marginBottom: "16px"
     }
 
-    render() {
+    return (
+        <div>
+            <Input className="clearfix input__left"
+                placeholder="Postcode"
+                name="postcode"
+                validations={{
+                    isExisty: true
+                }} >
+            </Input>
 
-        const buttonAdditionalStyle = {
-            marginBottom: "16px"
-        }
+            <button style={buttonAdditionalStyle} className="button button__secondary input__right">Find Address</button>
+            <br />
 
-        return (
-            <div>
-                <Input className="clearfix input__left"
-                    placeholder="Postcode"
-                    name="postcode"
-                    validations={{
-                        isExisty: true
-                    }} >
-                </Input>
+            <Input className="input__left"
+                placeholder="House Number"
+                name="house"
+                validations={{
+                    isExisty: true
+                }} />
+            <Input className="input__right"
+                placeholder="Street Name"
+                name="street"
+                validations={{
+                    isExisty: true
+                }} />
+            <Input className="input__left"
+                placeholder="Town or City"
+                name="town"
+                validations={{
+                    isExisty: true
+                }} />
 
-                <button style={buttonAdditionalStyle} className="button button__secondary input__right">Find Address</button>
-                <br />
-
-                <Input className="input__left"
-                    placeholder="House Number"
-                    name="house"
-                    validations={{
-                        isExisty: true
-                    }} />
-                <Input className="input__right"
-                    placeholder="Street Name"
-                    name="street"
-                    validations={{
-                        isExisty: true
-                    }} />
-                <Input className="input__left"
-                    placeholder="Town or City"
-                    name="town"
-                    validations={{
-                        isExisty: true
-                    }} />
-
-                <Input className="input__right"
-                    placeholder="Delivery Instructions"
-                    name="delivery" />
-            </div>
-        )
-    }
+            <Input className="input__right"
+                placeholder="Delivery Instructions"
+                name="delivery" />
+        </div>
+    )
 }
 
 export default BillingAddressInputs
--- src/js/components/formElements/CardDetailsInputs.jsx
+++ src/js/components/formElements/CardDetailsInputs.jsx
@@ -2,42 +2,39 @@ import React        from 'react'
 import Input        from './Input'
 import Formsy       from 'formsy-react'
 
-var CardDetailsInputs = React.createClass({
+const CardDetailsInputs = _ => {
 
-    render() {
+    return (
 
-        return (
+        <div>
+            <Input className="clearfix input__left input__left--large"
+                placeholder="The Long Number"
+                name="cardNumber"
+                validations={{
+                    isExisty: true
+                }} >
+            </Input>
 
-            <div>
-                <Input className="clearfix input__left input__left--large"
-                    placeholder="The Long Number"
-                    name="cardNumber"
-                    validations={{
-                        isExisty: true
-                    }} >
-                </Input>
-
-                <Input className="input__right input__right--small"
-                    placeholder="Expiry (MM/YY)"
-                    name="expiriy"
-                    validations={{
-                        isExisty: true
-                    }} />
-                <Input className="input__left input__left--large"
-                    placeholder="Cardholder's name"
-                    name="cardholder"
-                    validations={{
-                        isExisty: true
-                    }} />
-                <Input className="input__right input__right--small"
-                    placeholder="CVV"
-                    name="cvv"
-                    validations={{
-                        isExisty: true
-                    }} />
-            </div>
-        )
-    }
-})
+            <Input className="input__right input__right--small"
+                placeholder="Expiry (MM/YY)"
+                name="expiriy"
+                validations={{
+                    isExisty: true
+                }} />
+            <Input className="input__left input__left--large"
+                placeholder="Cardholder's name"
+                name="cardholder"
+                validations={{
+                    isExisty: true
+                }} />
+            <Input className="input__right input__right--small"
+                placeholder="CVV"
+                name="cvv"
+                validations={{
+                    isExisty: true
+                }} />
+        </div>
+    )
+}
 
 export default CardDetailsInputs
